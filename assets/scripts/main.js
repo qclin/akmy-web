4a45ad69271a8c7fa5dd24a06ddaafc627b0cfd3
Qiao Lin <qiao@goodpatch.com>
2016-10-05 12:54:08 -0400

    randomize option 2 with js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,9 @@
 $('ul').click((e) => {
 	$previewID = '#preview-'+e.target.id
+	$($previewID).siblings($('.preview')).hide()
 	$($previewID).show()
-	// $('#preview-2d').show()
-	randomDisplaceItem($previewID)
+	// randomDisplaceItem($previewID)
+	generateImageSizePostion($previewID)
 })
 
 $('ul').click((e) => {
@@ -14,12 +15,38 @@ $('li').hover(function(e){
 	$('#highlightGlow').insertAfter($('#IMG_'+e.target.id));
 })
 
+
+/* randomize postion - method 1 use CSS styling with preset positons 1-6 by matching selection index,
+short-coming index1 will always have class position1 */
+
 function randomDisplaceItem(previewID){
 	var position = 1
 	$(previewID).children('span').each(function(index, element){
 		var placeStyle = 'imageOverlay position'+position
 		$(this).addClass(placeStyle)
 		position ++
+	});
+}
+
+/*  randomize postion - method 2 use js to generate both randomize img size and positioning */
+function generateImageSizePostion(previewID){
+	var sizeArray=[{width: 200, height: 140},
+								{width: 320, height: 210},
+								{width: 180, height: 120},
+								{width: 175, height: 260}]
+	/// images are now stretch >> switch to randomize scale factor instead 
+	$(previewID).children('span').each(function(index, element){
+		$(this).addClass('imageOverlay')
+		var imgSize = sizeArray[Math.floor(Math.random() * sizeArray.length)];
+		var posX = (Math.random() * ($('#content-wrapper').width() - imgSize.width)).toFixed();
+		var posY = (Math.random() * ($('#content-wrapper').height() - imgSize.height)).toFixed();
+		$(this).children('img').css({
+			'width':imgSize.width+'px',
+			'height':imgSize.height+'px',
+			'left':posX+'px',
+			'top':posY+'px',
+			'position': 'absolute'
+		});
 	});
 }
 
