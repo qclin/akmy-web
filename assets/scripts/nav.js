84768f8b11d2f73d908cbfcb76fb86b42f48fb34
Qiao Lin <qiao@goodpatch.com>
2016-11-12 13:09:09 +0100

    fixed preview section

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -6,6 +6,7 @@ $( document ).on( "mobileinit", function() {
 var projectType = location.pathname.split('/')[1]
 var projectName = location.pathname.split('/')[2]
 var projectTypeList = ["2d", "3d", "fabrication", "model"]
+
 if(window.innerWidth > 768){
   if(projectTypeList.indexOf(projectType) > -1 ){
     $('#'+projectType).find('.navDropdownBox').show()
@@ -19,10 +20,17 @@ $('.label').click((e) => {
   $(e.target).next().show()
   $(e.target).parent().siblings().children('.navDropdownBox').hide()
   if(location.pathname == "/"){
-    $("#content").load("./views/components/preview.html #preview-"+$(e.target).parent().attr('id'), function(){
-      $previewID = '#preview-'+$(e.target).parent().attr('id');
-      generateImageSizePostion($previewID)
+    var selectedId = $(e.target).parent().attr('id')
+
+    $(`#preview-${selectedId}`).show(function(){
+      $(`#preview-${selectedId}`).siblings().hide()
+      generateImageSizePostion(`#preview-${selectedId}`)
     });
+
+    // $("#content").load("partials/preview.jade #preview-"+$(e.target).parent().attr('id'), function(){
+    //   $previewID = '#preview-'+$(e.target).parent().attr('id');
+    //   generateImageSizePostion($previewID)
+    // });
   }
 });
 
@@ -33,33 +41,25 @@ $('li').hover(function(e){
   }
 });
 
-function randomDisplaceItem(previewID){
-	var position = 1
-	$(previewID).children('span').each(function(index, element){
-		var placeStyle = 'imageOverlay position'+position
-		$(this).addClass(placeStyle)
-		position ++
-	});
-}
-
 /*  randomize postion - method 2 use js to generate both randomize img size and positioning */
 function generateImageSizePostion(previewID){
-	var sizeArray=[{width: 200, height: 140},
-								{width: 320, height: 210},
-								{width: 180, height: 120},
-								{width: 175, height: 260}]
-	/// images are now stretch >> switch to randomize scale factor instead
 	$(previewID).children('span').each(function(index, element){
-		$(this).addClass('imageOverlay')
-		var imgSize = sizeArray[Math.floor(Math.random() * sizeArray.length)];
-		var posX = (Math.random() * ($('#content-wrapper').width() - imgSize.width)).toFixed();
-		var posY = (Math.random() * ($('#content-wrapper').height() - imgSize.height)).toFixed();
+		$(this).addClass('imageOverlay');
+    var width = $(this).children('img').width();
+		var height = $(this).children('img').height();
+		var posX = (Math.random() * (window.innerWidth - width/5)).toFixed();
+		var posY = (Math.random() * (window.innerHeight - height/5)).toFixed();
+    if(height > width){
+      $(this).addClass('img-portrait');
+    }
+    $(this).css({
+      'left':posX+'px',
+      'top':posY+'px',
+      'position': 'absolute'
+    });
 		$(this).children('img').css({
-			'width':imgSize.width+'px',
-			'height':imgSize.height+'px',
-			'left':posX+'px',
-			'top':posY+'px',
-			'position': 'absolute'
+			'width':width/5+'px',
+			'height':height/5+'px',
 		});
 	});
 }
