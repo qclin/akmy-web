2e635b72e9bb9de12cfd3d7aebf1634ffc0f475e
Qiao Lin <qiao@goodpatch.com>
2016-11-15 20:21:22 +0100

    Update layout on project page WIP  - 3d pages, static textBox

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -5,7 +5,7 @@ $( document ).on( "mobileinit", function() {
 
 var projectType = location.pathname.split('/')[1]
 var projectName = location.pathname.split('/')[2]
-var projectTypeList = ["2d", "3d", "fabrication", "model"]
+var projectTypeList = ["2d", "fabrication", "model"]
 
 if(window.innerWidth > 768){
   if(projectTypeList.indexOf(projectType) > -1 ){
