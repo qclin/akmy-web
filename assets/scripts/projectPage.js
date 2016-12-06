62c4684f0eb928995acfc9a4dba8e0281e3fdabd
Qiao Lin <qiao@goodpatch.com>
2016-12-07 00:03:56 +0100

    svg solution for not loading objects in mobile

--- assets/scripts/projectPage.js
+++ assets/scripts/projectPage.js
@@ -14,14 +14,16 @@ $(document).ready(function(){
       return $(this).children('object').clone().prependTo('.img-modal');
     }else{
       $('.img-modal').css('top', 0);
-      $(e.target).clone().appendTo('.img-modal');
+      $(e.target).clone().prependTo('.img-modal');
     }
   });
+
   $('.img-modal').click(function(e){
     if(e.target.tagName != "IMG"){
       $('.img-modal').css('top', '100vh');
       $('.img-modal').find('img').remove();
       $('.img-modal').find('svg').remove();
+      $('.img-modal').find('object').remove();
       $('.img-modal').removeClass('portrait');
       $('.img-modal').removeClass('landscape');
     }
@@ -32,20 +34,22 @@ $(document).ready(function(){
 var current = 0;
 var imgList = $('.project-images')
 var maxIndex = imgList.length-1
-
 if(window.innerWidth <= 768){
 
-  // $('body').on('click', '.foreground', function(e){
-  //   debugger
-  //   var orientation = "portrait"
-  //   if(e.target.width > e.target.height){
-  //     orientation = "landscape"
-  //   }
-  //   $('.img-modal').addClass(orientation);
-  //   $('.img-modal').css('top', 0);
-  //   $(e.target).clone().appendTo('.img-modal');
-  // });
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    $('object').remove()
 
+    $('body').on('click', '.foreground img.mobile', function(e){
+      var orientation = "portrait"
+      if(e.target.width > e.target.height){
+        orientation = "landscape"
+      }
+      $('.img-modal').addClass(orientation);
+      $('.img-modal').css('top', 0);
+      var svgSource = $(this).attr('src')
+      $(`<object data=${svgSource}>`, {"data": svgSource, type: "image/svg+xml"}).prependTo('.img-modal');
+    });
+  }
   $(document).on('swipeleft', '.foreground',function(e){
     $(imgList).eq(current).removeClass('foreground')
     if(current == 0){
