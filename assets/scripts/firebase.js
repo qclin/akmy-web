8ff9d4de4b5038638f4bc67c4b6ce534cd977edd
Qiao Lin <qiaoclin@gmail.com>
2017-12-18 14:27:14 +0100

    archived here

---
+++ assets/scripts/firebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/4.6.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBrnqTW7DyMejkkyGetvvcJIZSDtfGtMzI",
+    authDomain: "akmy-web.firebaseapp.com",
+    databaseURL: "https://akmy-web.firebaseio.com",
+    projectId: "akmy-web",
+    storageBucket: "",
+    messagingSenderId: "540663567404"
+  };
+  firebase.initializeApp(config);
+</script>
